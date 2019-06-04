d06bc8eb47b4914d7fe11279f0cb5cf0eb474fd2
Jonas Apelfjord <jonas.apelfjord@appliedtechnology.se>
2019-06-04 17:55:46 +0200

    new endpoint for fetching data from mongo db

--- server/index.js
+++ server/index.js
@@ -1,33 +1,52 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const util = require('util');
-const cors = require('cors');
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const util = require("util");
+const cors = require("cors");
 
-const {PORT, mongo} = require('./config'); 
-const dbPath = require(mongo.dbPath)
+const { PORT, mongo } = require("./config");
+const dbPath = require(mongo.dbPath);
 const app = express();
+let data = {};
+
 
 app.use(cors());
 
-app.get('/number', (req, res, next) => {
-    res.send({number: 50});
-})
+app.get("/number", (req, res, next) => {
+  res.send({ number: 50 });
+});
 
-// app.get('/mongo', (req, res, next) => {
+app.get("/mongo", (req, res, next) => {
+    const dbName = "data";
+    const client = new MongoClient(mongo.url);
+    
+    client.connect(err => {
+        console.log("Connected to mongo server!");
+        
+    const db = client.db(dbName);
 
-//     const dbName = 'data';
-//     const client = new MongoClient(mongo.url)
+    fetchContent(db, () => {
+      db.collection("data")
+        .find({})
+        .toArray((err, items) => {
+          console.log(util.inspect(items, false, null, true));
+          data = items;
+        });
+      db.collection("data").deleteMany({});
+      client.close();
+    });
+  });
 
-//     client.connect(err => {
-//         console.log('Connected to mongo server!')
+  const fetchContent = (db, cb) => {
+    const collection = db.collection("data");
+    collection.insertMany(dbPath, (err, result) => {
+      console.log("Inserted data into the collection");
+      cb(result);
+    });
+  };
 
-//         const db = client.db(dbname);
+  res.send(data)
+});
 
-//         fetchContent(db, (e) => {
-//             db.collection('data')
-//         })
-//     })
-
-// })
-
-app.listen(PORT, () => {console.log('Server is running on port ' + PORT)})
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
