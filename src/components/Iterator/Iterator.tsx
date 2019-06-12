b52c00658dae7a36b9b3b39e135485ac0869ceec
Jonas Apelfjord <jonas.apelfjord@appliedtechnology.se>
2019-06-12 09:41:24 +0200

    turned ternary into switch/case in iterator

--- src/components/Iterator/Iterator.tsx
+++ src/components/Iterator/Iterator.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IteratorLi, IteratorUl } from './styled';
 import PhotoCard from "components/PhotoCard/PhotoCard";
+import VideoCard from "components/VideoCard/VideoCard";
 
 type OwnProps = {
   iterateOver: Array<any>,
@@ -20,21 +21,46 @@ class Iterator extends React.Component<OwnProps> {
 
     for (let i = 0; i < this.props.iterateOver.length; i++) {
       if (this.props.pickOut) {
-        printItems.push(
-          <IteratorLi 
-            key={i} 
-            align={this.props.alignRow || false} 
-            onClick={() => this.handleClick(i)}>
-              {this.props.iterateOver[i].type === 'image' ? 
-                (<PhotoCard element={this.props.iterateOver[i]}/>) : 
-                (this.props.iterateOver[i][this.props.pickOut])}
-          </IteratorLi>
-        );
+        switch (this.props.iterateOver[i].type) {
+          case 'image':
+            printItems.push(
+              <IteratorLi
+                key={i}
+                align={this.props.alignRow || false}
+                onClick={() => this.handleClick(i)}>
+                <PhotoCard element={this.props.iterateOver[i]} />
+              </IteratorLi>
+            )
+            break;
+            
+          case 'video':
+            printItems.push(
+              <IteratorLi
+                key={i}
+                align={this.props.alignRow || false}
+                onClick={() => this.handleClick(i)}>
+                <VideoCard element={this.props.iterateOver[i]} />
+              </IteratorLi>
+            )
+            break;
+
+          default:
+            printItems.push(
+              <IteratorLi
+                key={i}
+                align={this.props.alignRow || false}
+                onClick={() => this.handleClick(i)}>
+                {this.props.iterateOver[i][this.props.pickOut]}
+              </IteratorLi>
+            )
+            break;
+        }
+
       } else {
         printItems.push(
-          <IteratorLi 
-            key={i} 
-            align={this.props.alignRow || false} 
+          <IteratorLi
+            key={i}
+            align={this.props.alignRow || false}
             onClick={this.handleClick}>
             {this.props.iterateOver[i]}
           </IteratorLi>
